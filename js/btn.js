ff0f80915c9bafd1b5740fa03ba471d52e497177
maxew-dev <116647810+maxew-dev@users.noreply.github.com>
2023-09-06 01:05:31 +0200

    Add files via upload

--- js/btn.js
+++ js/btn.js
@@ -18,8 +18,30 @@ function initListeners()
     });
 
 
+    document.getElementById("btnInstall").addEventListener("click", async () =>{
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        const { outcome } = await deferredPrompt.userChoice;
+        // Optionally, send analytics event with outcome of user choice
+        console.log(`User response to the install prompt: ${outcome}`);
+        // We've used the prompt, and can't use it again, throw it away
+        deferredPrompt = null;
+    });
+
+
 
 }
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+  });
+
 
 function showNotification()
 {
