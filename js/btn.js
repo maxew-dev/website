db377ed2d73f9f143ff1a62956741037ebff6abd
maxew-dev <116647810+maxew-dev@users.noreply.github.com>
2023-09-06 00:32:37 +0200

    Add files via upload

---
+++ js/btn.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    initListeners();
+})
+
+function initListeners()
+{
+    document.getElementById("btnNotif").addEventListener("click", (ev)=>{
+        Notification.requestPermission().then((result) => {
+            if (result === "granted") {
+              showNotification();
+            }
+        });
+    });
+
+
+
+}
+
+function showNotification()
+{
+    const randomItem = Math.floor(Math.random() * 100);
+    const notifTitle = "Mission n°" + randomItem;
+    const notifBody = `Created by Max.`;
+    const notifImg = `icons/icon72.png`;
+    const options = {
+        body: notifBody,
+        icon: notifImg,
+    };
+
+    new Notification(notifTitle, options);
+    setTimeout(randomNotification, 30000);
+}
+
